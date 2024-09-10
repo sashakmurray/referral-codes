3a057e0618c294fa0e87875dbbd2bf22c84b25a2
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2024-09-09 23:03:54 -0700

    Add dropdown and text input for add link page

--- src/app/add_link/page.js
+++ src/app/add_link/page.js
@@ -1,3 +1,27 @@
+import styles from "../page.module.css";
+import Link from 'next/link';
+
 export default function Page() {
-    return <p>Add Link Page</p>;
-  }
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        Add link
+
+        <div className={styles.primary}>
+          <select>
+            <option value="Link">Referral link</option>
+            <option value="Code">Promo code</option>
+          </select>
+
+          <br />
+          <input type="text"/>
+
+          <br />
+          <button type="submit">Submit</button>
+        
+        </div>
+      </main>
+    </div>
+  );
+
+}
