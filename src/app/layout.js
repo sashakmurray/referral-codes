be80575aeb62d994e438d3f1da828fc9e054bda1
EwwPhysics <74149971+EwwPhysics@users.noreply.github.com>
2024-09-22 22:30:46 -0700

    Reorganize with separate ui folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
 import localFont from "next/font/local";
-import "./globals.css";
+import "./ui/globals.css";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "./ui/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "./ui/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
